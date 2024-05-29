7e45338fd536701b9345d15e90f90082b94db32c
AkifOzcelik <74510324+AkifOzcelik@users.noreply.github.com>
2024-05-29 03:22:23 +0200

    Update grindxpgobrrr.py

--- grindxpgobrrr.py
+++ grindxpgobrrr.py
@@ -4,6 +4,8 @@ import random
 
 keys = ['w', 'a', 's', 'd', 'p', 'space'] # creates a list for keys (p is for left click)
 
+exit_key = 'j'
+
 def press(key): # function to press keys
     keyboard.press(key)
     keyboard.block_key(key) 
@@ -16,5 +18,5 @@ time.sleep(2) # gives you a two seconds time span to alt-tab to the game
 while True:
     key = random.choice(keys) # picks a random key from the list
     press(key)
-    if keyboard.is_pressed('j'): # if you press the J it stops the script (you have to spam it)
+    if keyboard.is_pressed(exit_key): # if you press the J it stops the script (you have to spam it)
             break
